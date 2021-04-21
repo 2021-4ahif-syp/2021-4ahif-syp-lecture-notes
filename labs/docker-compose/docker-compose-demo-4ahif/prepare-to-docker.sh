05a708c1a6d23060257eb919136d2eb0bec74213
GIT_GLOBAL_USER_NAME <GIT_GLOBAL_MAIL>
2021-04-21 11:13:21 +0200

    add lab for docker-compose w/ quarkus

---
+++ labs/docker-compose/docker-compose-demo-4ahif/prepare-to-docker.sh
@@ -0,0 +1,1 @@
+mv target/docker-compose-demo-4ahif-1.1.0-SNAPSHOT-runner.jar ./src/main/docker/demo.jar
---
+++ labs/docker-compose/docker-compose-demo-4ahif/src/main/java/at/htl/GreetingResource.java
@@ -0,0 +1,29 @@
+package at.htl;
+
+import at.htl.entity.Person;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    EntityManager em;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello 4ahif";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public void createPerson(Person person) {
+        em.merge(person);
+    }
+}
